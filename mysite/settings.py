84133e05414c6f59431a57425a61ff093ec3a6af
RevelRies <ayzikov070@mail.ru>
2023-11-19 19:06:48 +0300

    dev

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,7 +91,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
