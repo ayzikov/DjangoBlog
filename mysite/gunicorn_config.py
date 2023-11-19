c69b1d40dadb9bd5e4e218e8444be8bb1b10fe80
RevelRies <ayzikov070@mail.ru>
2023-11-19 20:17:22 +0000

    configured NGINX

--- mysite/gunicorn_config.py
+++ mysite/gunicorn_config.py
@@ -1,6 +1,6 @@
 command = '/var/www/DjangoBlog/env/bin/gunicorn'
 python_path = '/var/www/DjangoBlog/mysite'
-bind = '0.0.0.0:8001'
+bind = '127.0.0.1:8001'
 workers = 5
 user = 'www'
 raw_env = 'DJANGO_SETTINGS_MODULE=mysite.settings'
