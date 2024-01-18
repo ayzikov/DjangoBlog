9e90c6fcd387b65c9ae9becdb464382eafb9a181
RevelRies <ayzikov070@mail.ru>
2024-01-18 20:27:56 +0300

    feat: implemented JWT authorization

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 
 from .views import PostList, PostDetail, UserPostList
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -10,6 +11,9 @@ urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("schema/redoc/", SpectacularRedocView.as_view(url_name="blog_api:schema"), name="redoc"),
     path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="blog_api:schema"), name="swagger-ui"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     re_path('^user/(?P<userid>.+)/$', UserPostList.as_view()),
 ]
 
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,12 +1,5 @@
-from django.http import Http404
-
 from rest_framework import generics
-from rest_framework import status
 from rest_framework import filters
-from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework.request import Request
-from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 
 from django_filters.rest_framework import DjangoFilterBackend
